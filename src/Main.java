743b49a2be11f9eb812de1b4cd94de53fef157ca
annie8881 <880801blue@gmail.com>
2017-09-30 14:41:28 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n,x;
+        System.out.print("Input");
+        n=in.nextInt();
+        System.out.printf("%d\n",n*9/5+32);
+
+
+
+    }
+}
